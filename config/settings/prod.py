e563fdee0bf5f2a1ea251675ab56d0f41c3f5769
정용선 <cjfrnjs123@naver.com>
2022-01-08 14:34:17 +0900

    database setting

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['15.165.115.183', 'fdxtesting.kro.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',
+        'USER': 'mysiteuser',
+        'PASSWORD': '123123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}

7cd2c097269447602c801c1940599dabc17cc904
정용선 <cjfrnjs123@naver.com>
2022-01-11 23:36:16 +0900

    must login

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,10 +1,12 @@
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from ..models import Question
 from django.db.models import Q, Count
 import logging
 logger = logging.getLogger('pybo')
 
+@login_required(login_url='common:login')
 def index(request):
     logger.info("INFO 레벨로 출력")
     """
@@ -39,6 +41,7 @@ def index(request):
     context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
+@login_required(login_url='common:login')
 def detail(request, question_id):
     """
     pybo 내용 출력
